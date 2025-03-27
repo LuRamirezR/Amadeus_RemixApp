4a7e9c70807ca5f9f6cf7f0b0695c923c4ed611a
Sebaspj7 <sebastian.perezj7@gmail.com>
2025-03-26 21:12:41 -0500

    [FEAT]:Se elimina reports del navbar y se cambia el nombre de los archivos

---
+++ app/routes/reports.tsx
@@ -0,0 +1,11 @@
+import ExportPDF from "~/components/graphics/reports-pdf";
+import Grafica from "../components/graphics/graphics";
+
+export default function Reportes() {
+  return (
+    <>
+      <Grafica />
+      <ExportPDF />
+    </>
+  );
+}
